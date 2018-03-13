aba59f56e6bdadb99f922a73a725bf7341045718
Cameron Reikes <cameronreikes@gmail.com>
2018-03-12 20:42:33 -0700

    Add main file and update gitignore
     - gitignore ignores output binaries now

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+// Gets a line of input
+func getInput(rdr *bufio.Reader) (string, error) {
+	out, err := rdr.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return out[:len(out)-1], nil
+}
+
+func main() {
+	// Define reader
+	reader := bufio.NewReader(os.Stdin)
+	text, err := getInput(reader)
+	if err != nil {
+		panic(err)
+	}
+	// Loop over until no more input
+	for ; text != ""; text, _ = getInput(reader) {
+		log.Println(text)
+	}
+}
